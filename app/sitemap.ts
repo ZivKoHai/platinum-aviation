6801b1da2718ed1541f0995a2595d7add3dee98a
zivkohai <ziv.kali656@gmail.com>
2025-02-17 14:32:07 +0200

    fix fetch error at build time sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,6 +2,8 @@ import type { MetadataRoute } from "next";
 import { getAllPosts } from "./lib/wordpress";
 import { globalConfig } from "@/global-config";
 
+export const dynamic = "force-dynamic";
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const staticPages = [
     "",
